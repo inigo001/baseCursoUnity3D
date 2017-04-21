f3625d32bfbb5c117e5a54936cf3c4b6b65d0cef
inigo001 <ivique001@gmail.com>
2017-04-21 16:53:55 +0200

    añadidos extras relacionados con KeyCodes.

---
+++ Assets/2_ProcesosUnity/2_KeyCode_scripts/_1_KeyCodeDebug.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _1_KeyCodeDebug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("PULSO S");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("MANTENGO PULSADO A");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Debug.Log("LEVANTO D");
+        }
+
+
+	}
+}
---
+++ Assets/2_ProcesosUnity/2_KeyCode_scripts/_2_KeyCodeMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _2_KeyCodeMove : MonoBehaviour {
+
+    public int speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * this.speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(-Vector3.up * this.speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * this.speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * this.speed * Time.deltaTime);
+        }
+	}
+}
